ea32497190abbb7d022a9b945f1edd29ec8d6856
senthilkumarfbss <msgtosenthil@gmail.com>
2024-08-26 00:52:19 +0530

    Added todo logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 import './css/style.css'
 
-console.log('from js-webpack')
+const input = document.querySelector('.item-input')
+const btn = document.querySelector('.item-btn')
+const list = document.querySelector('.item-list')
 
-const hello = () => console.log('Hello')
+function addItem (value){
+    const listItem = document.createElement('li')
+    listItem.textContent = value
+    return listItem
+}
+
+function removeItems(){
+    while(list.lastElementChild) {
+        list.removeChild(list.lastElementChild)
+    }
+}
+
+function updateUI(items){
+    const ul = document.createElement('ul')
+    items.map(item=>{
+        const li = addItem(item)
+        ul.appendChild(li)
+    })
+    removeItems()
+    list.appendChild(ul)
+}
+
+function clickHandler(){
+    let items = JSON.parse(localStorage.getItem('todos'))
+    if(!items) {
+        items = []
+    }
+    items.push(input.value)
+    input.value=''
+    localStorage.setItem('todos',JSON.stringify(items))
+
+    items = JSON.parse(localStorage.getItem('todos'))
+    updateUI(items)
+}
+
+btn.addEventListener('click', clickHandler)
